2c1837e11bd15985aade03b3b039195562ff450c
vk1408 <84286885+vk1408@users.noreply.github.com>
2022-02-19 19:27:48 +0100

    early commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace TextMethods
     {
         static void Main(string[] args)
         {
+            // test PascalCase()
             Console.WriteLine(UsefulTextUtils.PascalCase("NUMBER OF STUDENTS"));   
             
         }
